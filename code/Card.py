a2d2c333bbca365fbf4101039462017dfb165299
Ashley Flesouras <cimorene5@gmail.com>
2019-07-21 10:57:10 -0700

    Altered strings and order

--- code/Card.py
+++ code/Card.py
@@ -18,7 +18,7 @@ class Card(object):
       rank: integer 1-13
     """
 
-    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    suit_names = ["Spades", "Clubs", "Diamonds", "Hearts"]
     rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7", 
               "8", "9", "10", "Jack", "Queen", "King"]
 
